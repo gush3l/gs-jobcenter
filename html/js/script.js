17f87d04a506ca8bd76426b44e3f350339592da4
gush3l <ohmaiiiiiii@gmail.com>
2022-08-23 01:15:29 +0300

    Made more easily configurable, color changing triangles

--- html/js/script.js
+++ html/js/script.js
@@ -1,22 +1,22 @@
 const jsonList = {
-    "carousel_cell_0": "#30D252;#00A60D",
-    "carousel_cell_1": "#007FFF;#318CE7",
-    "carousel_cell_2": "#00FF40;#03C03C",
-    "carousel_cell_3": "#FF00BF;#FF00FF",
-    "carousel_cell_4": "#FFC72C;#FFD700",
-    "carousel_cell_5": "#fa8100;#ee6002",
-    "carousel_cell_6": "#00ce77;#02eeab",
-    "carousel_cell_7": "#fc3d09;#ff6337",
-    "carousel_cell_8": "#740ee1;#b13ef2"
+    "jobSlide_0": "#30D252;#00A60D",
+    "jobSlide_1": "#007FFF;#318CE7",
+    "jobSlide_2": "#00FF40;#03C03C",
+    "jobSlide_3": "#FF00BF;#FF00FF",
+    "jobSlide_4": "#FFC72C;#FFD700",
+    "jobSlide_5": "#fa8100;#ee6002",
+    "jobSlide_6": "#00ce77;#02eeab",
+    "jobSlide_7": "#fc3d09;#ff6337",
+    "jobSlide_8": "#740ee1;#b13ef2"
 }
 
 $(document).ready(function() {
-    var carousel = document.querySelector('.carousel');
-    var cells = carousel.querySelectorAll('.carousel__cell');
-    var cellCount = 9;
+    var jobSlides = document.querySelector('.jobsSlides');
+    var allJobSlides = jobSlides.querySelectorAll('.jobSlide');
+    var jobSlidesCount = allJobSlides.length;
     var selectedIndex = 0;
-    var cellWidth = carousel.offsetWidth;
-    var cellHeight = carousel.offsetHeight;
+    var cellWidth = jobSlides.offsetWidth;
+    var cellHeight = jobSlides.offsetHeight;
     var isHorizontal = true;
     var rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
     var radius, theta;
@@ -26,19 +26,21 @@ $(document).ready(function() {
 
     function rotateCarousel() {
         var angle = theta * selectedIndex * -1;
-        carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
+        jobSlides.style.transform = 'translateZ(' + -radius + 'px) ' +
             rotateFn + '(' + angle + 'deg)';
     }
 
     document.onkeyup = function(data) {
-        if (data.key == "ArrowRight"){
-            if (selectedIndex+1 == cellCount) return;
+        const nextKey = isHorizontal ? "ArrowRight" : "ArrowUp"
+        const prevKey = isHorizontal ? "ArrowLeft" : "ArrowDown"
+        if (data.key == nextKey){
+            if (selectedIndex+1 == jobSlidesCount) return;
             selectedIndex++;
             rotateCarousel();
             setVisibility();
             changeTriangleColors();
         }
-        if (data.key == "ArrowLeft"){
+        if (data.key == prevKey){
             if (selectedIndex-1 == -1) return; 
             selectedIndex--;
             rotateCarousel();
@@ -48,12 +50,12 @@ $(document).ready(function() {
     };
 
     function changeCarousel() {
-        theta = 360 / cellCount;
-        var cellSize = isHorizontal ? cellWidth : cellHeight;
-        radius = Math.round((cellSize / 2) / Math.tan(Math.PI / cellCount));
-        for (var i = 0; i < cells.length; i++) {
-            var cell = cells[i];
-            if (i < cellCount) {
+        theta = 360 / jobSlidesCount;
+        var jobSlideSize = isHorizontal ? cellWidth : cellHeight;
+        radius = Math.round((jobSlideSize / 2) / Math.tan(Math.PI / jobSlidesCount));
+        for (var i = 0; i < allJobSlides.length; i++) {
+            var cell = allJobSlides[i];
+            if (i < jobSlidesCount) {
                 cell.style.opacity = 1;
                 var cellAngle = theta * i;
                 cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
@@ -67,17 +69,17 @@ $(document).ready(function() {
     }
 
     function setVisibility(){
-        $("#carousel_cell_"+parseInt(cellCount-1)).fadeOut(200)
-        for (var i = 0; i < cellCount-1; i++){
-            $("#carousel_cell_"+i).fadeOut(200)
+        $("#jobSlide_"+parseInt(jobSlidesCount-1)).fadeOut(200)
+        for (var i = 0; i < jobSlidesCount-1; i++){
+            $("#jobSlide_"+i).fadeOut(200)
         }
-        if (selectedIndex != 0) $("#carousel_cell_"+parseInt(selectedIndex-1)).fadeIn(200)
-        $("#carousel_cell_"+parseInt(selectedIndex)).fadeIn(200)
-        $("#carousel_cell_"+parseInt(selectedIndex+1)).fadeIn(200)
+        if (selectedIndex != 0) $("#jobSlide_"+parseInt(selectedIndex-1)).fadeIn(200)
+        $("#jobSlide_"+parseInt(selectedIndex)).fadeIn(200)
+        $("#jobSlide_"+parseInt(selectedIndex+1)).fadeIn(200)
     }
 
     function changeTriangleColors() {
-        var colors = jsonList["carousel_cell_"+selectedIndex].split(";");
+        var colors = jsonList["jobSlide_"+selectedIndex].split(";");
         $(".triangle-down").css("border-top-color",colors[0])
         $(".triangle-up").css("border-bottom-color",colors[1])
     }
